43a372840c2c4fdc998d07f1e32e2e2adab3d10c
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-05-28 15:30:06 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        colors: {
+            "primaryOrange": "hsl(35, 77%, 62%)",
+            "primaryRed": "hsl(5, 85%, 63%)",
+
+            "neutralOffWhite": "hsl(36, 100%, 99%)",
+            "neutralBlue": "hsl(233, 8%, 79%)",
+            "neutralDarkBlue": "hsl(236, 13%, 42%)",
+            "neutralDarnessBlue": "hsl(240, 100%, 5%)"
+        },
+        fontFamily: {
+
+        }
+    },
+  },
+  plugins: [],
+}
